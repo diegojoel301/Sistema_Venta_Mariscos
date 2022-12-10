551f003b83a972e89431b37c0a0ed7b8c810f03a
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2022-12-10 13:05:40 -0800

    Add files via upload

---
+++ static/js/clock.js
@@ -0,0 +1,20 @@
+alert('1');
+const time=document.getElementById('time');
+
+const date = document.getElementById('date');
+
+
+
+const monthNames=["Enero", "Febrero", "Marzo", "Abril",
+                    "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+                    "Octubre", "Noviembre", "Diciembre"];
+
+alert(time+" "+date);
+const interval = setInterval(() => {
+    const local = new Date();
+    let day = local.getDate(),
+        month = local.getMonth(),
+        year = local.getFullYear();
+    time.innerHTML= local.toLocaleTimeString();
+    date.innerHTML=`${day} ${monthNames[month]} ${year}`;
+}, 1000);
